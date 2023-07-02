810bfa3b57ee397660c255831b4fab12bd9f8ed0
NRAS App <nrasapp@outlook.com>
2023-07-02 17:55:43 +0700

    Add files via upload

---
+++ Rectangle Calculator/main.py
@@ -0,0 +1,27 @@
+from math import sqrt
+
+print("Rectangle Calculator by NRAS App\n")
+
+def diagonal():
+    diagonal = sqrt(side_length_a ** 2 + side_length_b ** 2)
+    print("Diagonal:", diagonal)
+
+def perimeter():
+    perimeter = side_length_a * 2 + side_length_b * 2
+    print("Perimeter:", perimeter)
+
+def area():
+    area = side_length_a * side_length_b
+    print("Area:", area)
+
+while True:
+    side_length_a = int(input("Side length A: "))
+    side_length_b = int(input("Side length B: "))
+    print("\nResults:\n")
+    diagonal()
+    perimeter()
+    area()
+    ctn = input("\nDo you want to continue? (y/n): ")
+    if ctn == "n" or ctn == "n":
+        print("Thank you")
+        break
